c76d0559446a03d5d383a4940d560b3882930c5a
Ramada Nguyen <ramadawin212@gmail.com>
2022-09-27 17:59:31 -0500

    commit tree 6db8446c35ae9c2f41200c6359d9e0b1c2700bad

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+function writeCards (name , event) {
+let messages = []
+for (let i = 0; i < name.length; i++){
+    messages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`) 
+}
+return messages
+}
+function countDown(number){
+    while (number > -1){
+        console.log(number--)
+    }
+}
